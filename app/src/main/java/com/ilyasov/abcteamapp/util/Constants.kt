2c602b726c90ed6168a6aa494f3aa4a111521f50
Timur_Q <ilyasovtimur2001.work@gmail.com>
2021-10-03 23:04:35 +0300

    Add some Constants

---
+++ app/src/main/java/com/ilyasov/abcteamapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.ilyasov.abcteamapp.util
+
+const val BASE_MOVE_SPEED = 2000000000L
+const val BASE_TIME_TO_RESPAWN = 2500000000L
+const val APP_PREFERENCES = "settings"
+const val SAVED_SCORE = "__SAVED_SCORE__"
